7c4bf4eb96ff29ffad9621978644d6ed0e1e6bbb
Kevin Oliver <kevinoliver@gmail.com>
2024-03-29 08:14:12 -0700

    Basic event handling
    Listens for click events and finds ones that are on mailto: <A> elements

--- gmailto Extension/Resources/content.js
+++ gmailto Extension/Resources/content.js
@@ -1,7 +1,17 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+console.log("gmailto: loaded");
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+window.addEventListener("click", function(event) {
+    if (event.target.tagName != "A") return;
+    
+    // See: https://stackoverflow.com/questions/29223502/how-to-get-href-of-anchor-when-the-event-target-is-htmlimageelement
+    const anchor = event.target.closest("a");
+    if (!anchor) return;
+    const href = event.target.getAttribute("href");
+    if (!href || !href.startsWith("mailto:")) return;
+
+    console.log("gmailto: mailto found: " + href);
+    
+    // stop the click and the propagation
+    event.preventDefault();
+    event.stopPropagation();
 });
