f061b54f86d5d09676b13a161154ee765f56dff2
Kevin Oliver <kevinoliver@gmail.com>
2024-03-29 08:22:21 -0700

    Use the correct javascript element
    Use the element found by `closest` instead of the `event's`

--- gmailto Extension/Resources/content.js
+++ gmailto Extension/Resources/content.js
@@ -6,7 +6,7 @@ window.addEventListener("click", function(event) {
     // See: https://stackoverflow.com/questions/29223502/how-to-get-href-of-anchor-when-the-event-target-is-htmlimageelement
     const anchor = event.target.closest("a");
     if (!anchor) return;
-    const href = event.target.getAttribute("href");
+    const href = anchor.target.getAttribute("href");
     if (!href || !href.startsWith("mailto:")) return;
 
     console.log("gmailto: mailto found: " + href);
